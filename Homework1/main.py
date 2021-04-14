8bb2d8bcaa5fe9bb8de6c52614e384ac76eacc27
gatchigat <80865346+gatchigat@users.noreply.github.com>
2021-04-14 11:44:45 +0800

    initial commit

---
+++ Homework1/main.py
@@ -0,0 +1,32 @@
+'''Pirple  Homework #1
+Details:
+ 
+What's your favorite song?
+
+Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
+
+In your text editor, create an empty file and name it main.py
+
+Hello by Lionel Richie'''
+
+
+# Defining Variables
+
+Title = "Hello"  # Song title
+Artist = "Lionel Richie"  # Artist's Name
+TrackNo = 11  # Track Number
+Album = "Back To Front"  # Album Name
+Year = 1992  # Year released
+Genre = "Pop"  # Song genre
+Duration = 328  # Song duration in seconds
+Publisher = "Motown"  # Publisher Name
+
+# Print Variables
+print('Title: ' + Title)
+print('Artist: ' + Artist)
+print('TrackNo: ' + str(TrackNo))
+print('Album: ' + Album)
+print('Release year: ' + str(Year))
+print('Genre: ' + Genre)
+print('Length: ' + str(Duration) + ' seconds')
+print('Publisher: ' + Publisher)
